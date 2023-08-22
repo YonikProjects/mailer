33b7c5ccd111c74491e9520d43f3b1e505799c6c
YonikProjects <95275791+YonikProjects@users.noreply.github.com>
2023-08-23 01:11:26 +0300

    Turnstile

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
+const { turnstile } = require("express-turnstile");
 
 const app = express();
 const PORT = 3000;
@@ -9,40 +10,44 @@ const PORT = 3000;
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post("/pform", async (req, res) => {
-  let formData = req.body;
+app.post(
+  "/pform",
+  turnstile.validate(process.env.TURNSTILE),
+  async (req, res) => {
+    let formData = req.body;
 
-  let transporter = nodemailer.createTransport({
-    host: process.env.SMTP_Server,
-    port: process.env.SMTP_Port,
-    // secure: true,
-    auth: {
-      user: process.env.SMTP_User,
-      pass: process.env.SMTP_Pass,
-    },
-  });
+    let transporter = nodemailer.createTransport({
+      host: process.env.SMTP_Server,
+      port: process.env.SMTP_Port,
+      // secure: true,
+      auth: {
+        user: process.env.SMTP_User,
+        pass: process.env.SMTP_Pass,
+      },
+    });
 
-  let mailOptions = {
-    from: process.env.From,
-    to: process.env.To, // Your email where you want to receive the forwarded data
-    subject: "New Form Submission",
-    text: `
+    let mailOptions = {
+      from: process.env.From,
+      to: process.env.To, // Your email where you want to receive the forwarded data
+      subject: "New Form Submission",
+      text: `
             Name: ${formData.name}
             Email: ${formData.email}
             Phone: ${formData.phone}
             Subject: ${formData.subject}
             Message: ${formData.message}
         `,
-  };
+    };
 
-  try {
-    await transporter.sendMail(mailOptions);
-    res.json({ success: true });
-  } catch (error) {
-    console.error("Error sending email:", error);
-    res.status(500).json({ success: false });
+    try {
+      await transporter.sendMail(mailOptions);
+      res.json({ success: true });
+    } catch (error) {
+      console.error("Error sending email:", error);
+      res.status(500).json({ success: false });
+    }
   }
-});
+);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
