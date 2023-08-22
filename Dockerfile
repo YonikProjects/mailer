fd5bf214e16db6f38d9b255c7393d9471d430a31
YonikProjects <95275791+YonikProjects@users.noreply.github.com>
2023-08-22 21:36:30 +0300

    First

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Use an official Node.js runtime as the base image
+FROM node:16
+
+# Set the working directory in the container to /app
+WORKDIR /app
+
+# Copy package.json and package-lock.json to the container's working directory
+COPY package*.json ./
+
+# Install the application dependencies inside the container
+RUN npm install
+
+# Copy the entire project to the container's working directory
+COPY . .
+
+# Expose port 3000 for the app to listen on
+EXPOSE 3000
+
+# Define the command to run the app
+CMD [ "node", "index.js" ]
---
+++ index.js
@@ -0,0 +1,49 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+
+const app = express();
+const PORT = 3000;
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/pform", async (req, res) => {
+  let formData = req.body;
+
+  let transporter = nodemailer.createTransport({
+    host: process.env.SMTP_Server,
+    port: process.env.SMTP_Port,
+    secure: true,
+    auth: {
+      user: process.env.SMTP_User,
+      pass: process.env.SMTP_Pass,
+    },
+  });
+
+  let mailOptions = {
+    from: process.env.From,
+    to: process.env.To, // Your email where you want to receive the forwarded data
+    subject: "New Form Submission",
+    text: `
+            Name: ${formData.name}
+            Email: ${formData.email}
+            Phone: ${formData.phone}
+            Subject: ${formData.subject}
+            Message: ${formData.message}
+        `,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    res.json({ success: true });
+  } catch (error) {
+    console.error("Error sending email:", error);
+    res.status(500).json({ success: false });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
