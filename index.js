5f4aaad2b67a39d798272cd3db3d92f1a19006b3
YonikProjects <95275791+YonikProjects@users.noreply.github.com>
2023-08-23 12:38:13 +0300

    Another try

--- index.js
+++ index.js
@@ -3,16 +3,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
 const cors = require("cors");
-let fetch;
-import("node-fetch").then((module) => {
-  fetch = module.default;
-});
-let allowlist = [
+const axios = require("axios"); // Use Axios
+
+const allowlist = [
   "https://dmitrypol.com",
   "http://localhost:5173",
   "https://test.dmitrypol.com",
   "https://portfolio.dmitrypol.com",
 ];
+
 const corsOptions = {
   origin: function (origin, callback) {
     if (allowlist.indexOf(origin) !== -1 || !origin) {
@@ -22,51 +21,48 @@ const corsOptions = {
     }
   },
 };
+
 const app = express();
 const PORT = 3000;
+
 const verifyTokenMiddleware = async (req, res, next) => {
-  console.log(JSON.stringify(req));
+  console.log(req.body);
   const token = req.body["cf-turnstile-response"];
   const url = "https://challenges.cloudflare.com/turnstile/v0/siteverify";
   const data = {
     secret: process.env.TURNSTILE,
     response: token,
   };
+
   try {
-    const result = await fetch(url, {
-      body: JSON.stringify(data),
-      method: "POST",
+    const response = await axios.post(url, data, {
       headers: {
         "Content-Type": "application/json",
       },
     });
 
-    const outcome = await result.json();
-    console.log(outcome);
-    if (outcome.success) {
+    console.log(response.data);
+    if (response.data.success) {
       next(); // Continue to the next middleware/route if successful
     } else {
-      // Handle the verification failure
       res.status(401).json({ error: "Verification failed" });
     }
   } catch (error) {
-    // Handle any other errors that might occur
-    console.error("Error verifying token:", error);
+    console.error("Error verifying token:", error.message);
     res.status(500).json({ error: "Internal server error" });
   }
 };
 
-// Middleware
 app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(verifyTokenMiddleware);
+
 app.post("/pform", async (req, res) => {
   let formData = req.body;
   let transporter = nodemailer.createTransport({
     host: process.env.SMTP_Server,
     port: process.env.SMTP_Port,
-    // secure: true,
     auth: {
       user: process.env.SMTP_User,
       pass: process.env.SMTP_Pass,
@@ -75,7 +71,7 @@ app.post("/pform", async (req, res) => {
 
   let mailOptions = {
     from: process.env.From,
-    to: process.env.To, // Your email where you want to receive the forwarded data
+    to: process.env.To,
     subject: "New Form Submission",
     text: `
             Name: ${formData.name}
@@ -90,7 +86,7 @@ app.post("/pform", async (req, res) => {
     await transporter.sendMail(mailOptions);
     res.json({ success: true });
   } catch (error) {
-    console.error("Error sending email:", error);
+    console.error("Error sending email:", error.message);
     res.status(500).json({ success: false });
   }
 });
